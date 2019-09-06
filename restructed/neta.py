364287d63035d19cfe7c9130694363193c30aef8
Kcabo <37576764+kcabo@users.noreply.github.com>
2019-09-06 19:25:01 +0900

    jj

--- restructed/neta.py
+++ restructed/neta.py
@@ -3,4 +3,8 @@ symbols = "🇦🇧🇨🇩🇪🇫🇬🇭🇮🇯🇰🇱🇲🇳🇴🇵🇶�
 
 def pop_regional_indicator(text):
     code_list = [ord(c) for c in text]
-    return symbols[sum(code_list)%26] + symbols[(sum(code_list)//26)%26]
+    strings = symbols[sum(code_list)%26] + symbols[(sum(code_list)//26)%26]
+    if strings == "🇯🇵":
+        return strings + ' ←おめでとう！日本だ！'
+    else:
+        return strings
