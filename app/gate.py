b69b6df609fd1f9483e8b8923df3e6d3447de5e6
Kcabo <k7cabo@gmail.com>
2020-10-19 01:23:15 +0900

    デバッグへの最適化、ユーザー認証

--- app/gate.py
+++ app/gate.py
@@ -1,24 +1,21 @@
 from app.models import db, User
 from app.redis_setup import conn
 
-class UserState:
-    def __init__(self, line_id, menu_id=0, authorized=True):
-        self.line_id = line_id
-        self.menu_id = menu_id
-        self.authorized = authorized
+class UserNotFound(Exception):
+    def __init__(self, line_id):
+        print(f'{line_id}による不明なアクセスを検知')
 
-    def set_current_menu_id(new_menu_id):
-        conn.set(self.line_id, new_menu_id)
+
+def set_current_menu_id(line_id, new_menu_id):
+    conn.set(line_id, new_menu_id)
 
 
 def validate_user(line_id):
     # line_idがNoneのときに注意
     menu_id = conn.get(line_id)
-    if menu_id: return UserState(line_id, menu_id)
+    if menu_id: return menu_id
 
     # Redis上から見つからなかったらテーブルから探査
-    user = db.session.query(User.role).filter_by(lineid=line_id).one_or_none()
-    if user.role in ['EDITOR', 'ADMIN']:
-        return UserState(line_id)
-    else:
-        return UserState(line_id, authorized=False)
+    user = db.session.query(User.userid).filter_by(lineid=line_id).one_or_none()
+    if user: return 0
+    else: raise UserNotFound(line_id)
