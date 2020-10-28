070753e400016923e5ef8133379cf85aec3dabd9
Kcabo <37576764+kcabo@users.noreply.github.com>
2020-10-28 21:47:41 +0900

    EventクラスをLineapiモジュールに移動

--- app/webhook/handler.py
+++ app/webhook/handler.py
@@ -1,65 +1,30 @@
 import datetime
 from app.models import db
-from app import gate, line_api
+from app.line_api import Event, notify
+from app.gate import validate_user, UserNotFound
 from app.webhook import dispatcher, humor
-
-
-class Event:
-    def __init__(self, event_json):
-        self.type = event_json.get('type')
-        self.reply_token = event_json.get('replyToken')
-        self.line_id = event_json.get('source', {'userId': None}).get('userId') # sourceキーがないときもある
-        self.text = event_json.get('message', {'text': None}).get('text')
-        self.postback_data = event_json.get('postback', {'data': None}).get('data')
-        self.menu_id = 0
-        print(event_json.get('postback', None), 'ポストバックデータ見せる')
-
-    def reply(self, msg_list):
-        line_api.post_reply(self.reply_token, msg_list)
-
-
-    def send_text(self, *texts):
-        msg_list = [{'type': 'text', 'text': t} for t in texts if t is not None]
-        self.reply(msg_list)
-
-
-    def send_flex(self, alt_text, flex_msgs: list):
-        msg_list = [{
-            'type': 'flex',
-            'altText': alt_text,
-            'contents': flex
-        } for flex in flex_msgs if flex is not None]
-        self.reply(msg_list)
-
-
-    def reply_with_icon(self, msg_dic):
-        url = 'https://static.thenounproject.com/png/335121-200.png'
-        msg_dic['sender'] = {
-            'iconUrl': url
-        }
-        self.reply([msg_dic])
 
 
 def handle(event_json):
     event = Event(event_json)
 
     if event.type == 'follow':
-        line_api.notify(f'{event.line_id}から友達登録されました')
+        notify(f'{event.line_id}から友達登録されました')
     elif event.type == 'unfollow':
-        line_api.notify(f'{event.line_id}からブロックされました')
+        notify(f'{event.line_id}からブロックされました')
     elif event.type not in ['message', 'postback']:
         return 0
 
     # 操作中のメニューIDを取得
     try:
-        event.menu_id = gate.validate_user(event.line_id)
+        event.menu_id = validate_user(event.line_id)
         if event.type == 'message':
             receive_message(event)
         else:
             receive_postback(event)
 
     # LINEIDによる検索にヒットしない=未登録
-    except gate.UserNotFound:
+    except UserNotFound:
         # TODO: まだ友達の人に権限を与えるためのフォームを送信する→LIFF
         msg = 'まだユーザー登録されておりません。管理者に登録をお願いしてください。'
         event.send_text(msg)
