ae8e29f99e185854735bb0e4b0ae112b19e6bc87
Kcabo <37576764+kcabo@users.noreply.github.com>
2020-10-28 19:32:22 +0900

    ローカルでdot-env を導入

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from app.application import app
 
 if __name__ == '__main__':
+    # read environment variable
+    from os.path import join, dirname
+    from dotenv import load_dotenv
+    dotenv_path = join(dirname(__file__), 'safe', '.env')
+    load_dotenv(dotenv_path)
+
+    # run flask locally
+    # WARNING: do not exec in a product environment
+    from app.application import app
     app.run()
