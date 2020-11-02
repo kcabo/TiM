adcee64954703474776c17a2ed0a97357bff4863
Kcabo <k7cabo@gmail.com>
2020-11-02 21:04:54 +0900

    LIFFのルーティングを追加

--- app/application.py
+++ app/application.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 from app.models import db
 from app.webhook import handler
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='liff/templates', static_folder='liff/static')
 app.config.from_object('app.config.Config')
 db.init_app(app)
 
@@ -11,13 +11,20 @@ db.init_app(app)
 def hello():
     return 'Hi'
 
-@app.route('/callback', methods=['POST'])
-def callback():
+
+@app.route('/webhook', methods=['POST'])
+def webhook_handler():
     data = request.get_json()
     # events内にWebhookイベントのリストが格納されている
     for event_json in data['events']:
         handler.handle(event_json)
     return '200 OK'
+
+
+@app.route('/liff/menu')
+def menu_page():
+    return render_template('menu.html', hello='こん')
+
 
 @app.route('/create')
 def create():
