c6df59e2924e65313a9fc62f541be0d2ebffb391
Kcabo <k7cabo@gmail.com>
2020-11-05 04:20:07 +0900

    可変フォーム追加

--- app/liff/static/js/menu.js
+++ app/liff/static/js/menu.js
@@ -1,40 +1,23 @@
 
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   delimiters: ['${', '}'],
   data: {
-    message: 'これはヘッドメッセージ',
-    tweet: 'yeah',
-    cycle: '3:00',
-    height:"40px"
+    category: 'Swim',
+  },
+  methods: {
+
   },
   computed: {
-    styles: function () {
-      return {
-          "height": this.height,
-        }
-    }
+
   },
   watch: {
-    tweet: function () {
-      this.height = "auto";
-      this.$nextTick(()=>{
-        this.height = this.$refs.area.scrollHeight + 'px';
-      })
-    },
-
-    cycle: function () {
-      const textarea = this.$refs.area2;
+    category: function () {
+      const textarea = this.$refs.category_area;
       textarea.style.height = 'auto';
-      this.$nextTick(()=>{
+      this.$nextTick(() => {
         textarea.style.height = textarea.scrollHeight + 'px'
       });
-      // const resetHeight = new Promise(function(resolve) {
-      //   resolve(textarea.style.height = 'auto')
-      // });
-      // resetHeight.then(function(){
-      //   textarea.style.height = textarea.scrollHeight + 'px'
-      // });
     }
   }
 });
