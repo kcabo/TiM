4db84e3bdbb96102e36988ab2ee93e82d8982c1b
Kcabo <k7cabo@gmail.com>
2020-11-05 17:04:57 +0900

    LIFF修正

--- app/liff/static/js/menu.js
+++ app/liff/static/js/menu.js
@@ -5,16 +5,17 @@ Vue.component('form-component', {
       showChoices: false
     }
   },
-  props: {
-    name: String,
-    content: String,
-    choices: Array
-  },
+  // props: {
+  //   name: String,
+  //   content: String,
+  //   choices: Array
+  // },
+  props: ['name', 'content', 'choices'],
   template: `
   <div class="form-unit">
     <div class="filler"> </div>
     <label class="form-name" for="ta1">{{name}}</label>
-    <div class="toggle-button" v-bind:class="{active: showChoices}" v-on:click="showChoices=!showChoices">
+    <div class="toggle-button" v-bind:class="{active: showChoices}" v-on:touchstart="showChoices=!showChoices" v-on:click="showChoices=!showChoices">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
         <defs/>
         <path fill="#AFBFC9" d="M16 2h-4.18C11.4.84 10.3 0 9 0 7.7 0 6.6.84 6.18 2H2C.9 2 0 2.9 0 4v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM9 2c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H4v-2h7v2zm3-4H4v-2h10v2zm0-4H4V6h10v2z"/>
