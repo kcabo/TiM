98f46e8a64933df69560e0c5e9c988446e88ea29
kcabo <37576764+kcabo@users.noreply.github.com>
2023-08-27 16:14:59 +0900

    update base files for Azure

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-# run flask locally
-if __name__ == '__main__':
-    # read environment variables from local file
-    dotenv_path = join(dirname(__file__), 'safe', '.env')
-    load_dotenv(dotenv_path)
-
+if __name__ == "__main__":
     # do not exec on production environment
     from app.application import app
+
     app.run()
